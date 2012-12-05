3ba7ce6b39e16b341c1a75f8779ded691fa55430
Victor Sidorov <ostlander@gmail.com>
2012-12-05 20:49:51 +0400

    No line ending in urls.py .

--- ushare/files/urls.py
+++ ushare/files/urls.py
@@ -6,5 +6,5 @@ from .views import create_view, ImageFileDetailView
 
 urlpatterns = patterns('',
     url(r'^upload/$', create_view, name='upload'),
-    url(r'^(?P<slug>[-_\w]+)/(?P<filename>[-_.%\w]+)?$', ImageFileDetailView.as_view(), name='detail'),
+    url(r'^(?P<slug>[-_\w]+)/(?P<filename>[-_.%\w]+)?', ImageFileDetailView.as_view(), name='detail'),
 )
