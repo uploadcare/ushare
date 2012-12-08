b91a036ba3e7653509bb002ad8f8f29d996c9cfe
Victor Sidorov <ostlander@gmail.com>
2012-12-09 03:43:57 +0400

    'Upload more' and 'Try again buttons'.

--- staticfiles/javascripts/base.js
+++ staticfiles/javascripts/base.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
 
 	var initClipBoard = function() {
-		var clip = new ZeroClipboard.Client();
-		clip.setHandCursor(true);
-		clip.glue($('@clipboard-copy-button').get(0));
+		if (!window.clip) {
+			window.clip = new ZeroClipboard.Client();
+			clip.setHandCursor(true);
+			clip.glue($('@clipboard-copy-button').get(0));
 
-		clip.addEventListener('onMouseDown', function(client) {
-			clip.setText($('@file-url').val());
-		});
+			clip.addEventListener('onMouseDown', function(client) {
+				clip.setText($('@file-url').val());
+			});
+		}
+		else {
+			window.clip.show();
+		};
 	};
 
 	var uploadProgress = function(loaded, total, reserved) {
 		reserved = reserved || 0;
 		var width = loaded / total
 		$('@progress-bar .bar').width(((loaded / total) * 100 - reserved) + '%');
+	};
+
+	var uploadReset = function() {
+		$('@progress-bar')
+		.find('.progress').removeClass('progress-success progress-danger').addClass('progress-striped active')
+			.find('.bar').width(0);
+		$('@progress-bar, @upload-success, @upload-fail').addClass('hidden');
+		$('@upload-form').removeClass('hidden');
+		if (window.clip) window.clip.hide();
 	};
 
 	var uploadComplete = function(file_url, errors) {
@@ -24,7 +38,7 @@ $(document).ready(function() {
 		$('.bar', $progress_bar).width(100 + '%');
 
 		setTimeout(function() {
-			$progress_bar.removeClass('progress-striped active').addClass(result_class);
+			$('.progress', $progress_bar).removeClass('progress-striped active').addClass(result_class);
 			setTimeout(function() {
 				$progress_bar.addClass('hidden');
 				if (file_url) {
@@ -38,7 +52,9 @@ $(document).ready(function() {
 			}, 500);
 	};
 
-	$($('@uploadcare-uploader').data('widget').uploaders.file)
+	var widget = $('@uploadcare-uploader').data('widget');
+
+	$(widget.uploaders.file)
 		.on('uploadcare.api.uploader.start', function(e) {
 			$('@upload-form').addClass('hidden');
 			$('@progress-bar').removeClass('hidden');
@@ -46,6 +62,17 @@ $(document).ready(function() {
 		.on('uploadcare.api.uploader.progress', function(e){
 			uploadProgress(e.target.loaded, e.target.fileSize, 10);
 		});
+
+	$('@upload-cancel-button').click(function() {
+		$(widget.template).trigger('uploadcare.widget.template.cancel');
+		uploadReset()
+	});
+
+	$('@upload-reset').click(function(e) {
+		e.preventDefault();
+		$(widget.template).trigger('uploadcare.widget.template.remove');
+		uploadReset();
+	});
 
 	$('@upload-form').submit(function(e){
 		e.preventDefault();
@@ -65,7 +92,9 @@ $(document).ready(function() {
 	});
 
 	$('@uploadcare-uploader').change(function() {
-		$(this).closest('form').submit();
+		if ($(this).val()) {
+			$(this).closest('form').submit();
+		};
 	});
 
 });
