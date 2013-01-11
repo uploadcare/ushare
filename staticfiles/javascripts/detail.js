7b35236281ab04b11615a558fc36ad2d03963a79
Victor Sidorov <ostlander@gmail.com>
2013-01-11 17:37:10 +0400

    Correct file alignment; slider for images implemented.

---
+++ staticfiles/javascripts/detail.js
@@ -0,0 +1,14 @@
+$(function() {
+	$('@file-info-button').click(function() {
+		$('@file-info').hide();
+		$('@file-image').hide('slide', {direction: 'left'}, 200, function() {
+			$('@file-info').show('slide', {direction: 'right'}, 200);
+		});
+	});
+
+	$('@file-image-button').click(function() {
+		$('@file-info').hide('slide', {direction: 'right'}, 200, function() {
+			$('@file-image').show('slide', {direction: 'left'}, 200);
+		});
+	});
+});
