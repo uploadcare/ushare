fc648f7c9fb2ab8fd9d5f973de52c0a9f91daf1e
Victor Sidorov <ostlander@gmail.com>
2013-01-17 21:22:14 +0400

    Oembed roll-back

--- ushare/files/urls.py
+++ ushare/files/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
-from .views import create_view, FileDetailView, FileOEmbedView
+from .views import create_view, FileDetailView
 
 
 
 urlpatterns = patterns('',
     url(r'^upload/$', create_view, name='upload'),
     url(r'^(?P<encoded_id>[%s]+)/$' % settings.SHORT_URL_ALPHABET, FileDetailView.as_view(), name='detail'),
-    url(r'^(?P<encoded_id>[%s]+)/oembed/$' % settings.SHORT_URL_ALPHABET, FileOEmbedView.as_view(), name='oembed'),
 )
