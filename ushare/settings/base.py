78730c5825662bbe30bb4c237ef2a3fd80d7a811
Victor Sidorov <ostlander@gmail.com>
2013-01-22 19:48:45 +0400

    Server-side mobile browsers detection.

--- ushare/settings/base.py
+++ ushare/settings/base.py
@@ -107,6 +107,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'minidetector.Middleware',
 )
 
 ROOT_URLCONF = 'ushare.urls'
