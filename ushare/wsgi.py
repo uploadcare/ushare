7b2b5c0080cbf4905f810b55a032ca7e29acbe2a
Dmitry Mukhin <dmitry.s.mukhin@gmail.com>
2013-12-26 22:03:32 +0400

    fix uwsgi app

--- ushare/wsgi.py
+++ ushare/wsgi.py
@@ -14,10 +14,11 @@ framework.
 
 """
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ushare.settings")
+
 
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ushare.settings")
 
 application = Cling(get_wsgi_application())
